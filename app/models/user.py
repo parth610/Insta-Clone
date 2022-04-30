76daa2e653c1924088c08f7647c992ef44a6a6ed
Gabriel Sitorus <sitorusgg@gmail.com>
2022-05-01 05:49:00 +0700

    added usesr model with its labels that was added to our db.diagram

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    private = db.Column(db.Boolean, nullable=False  )
 
     @property
     def password(self):
@@ -26,5 +30,8 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'private': self.private, 
         }
