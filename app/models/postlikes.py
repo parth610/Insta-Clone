a015e9c9ab1d0e5a679e35626c472076108c188e
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-02 13:07:47 -0500

    created postlikes.py model

---
+++ app/models/postlikes.py
@@ -0,0 +1,18 @@
+from .db import db
+
+class PostLike(db.Model):
+    __tablename__ = 'post_likes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='postlikes')
+    post = db.relationship('Post', back_populates='postlikes')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'post_id': self.post_id
+        }
