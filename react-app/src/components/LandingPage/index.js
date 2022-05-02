d8b3009a99534d9efe3ae865f8bbd168c62efb92
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-02 16:56:45 -0500

    login form styles

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import LoginForm from '../auth/LoginForm';
 import SignUpForm from '../auth/SignUpForm';
 import homeVectorImage from '../../images/Flixtagram_home_vector.png';
+import instaLogo from '../../images/instagram_logo.png'
+import './LandingPage.css'
 
 const LandingPage = () => {
-
   const [showSignUp, setShowSignUp] = useState(false);
 
-
-
   return (
-    <div className='landing-page-container-LEFT'>
-      <div className='image-container'>
-        <img src={homeVectorImage}></img>
+    <div className='landing-page-container'>
+      <div className='image-container-LEFT'>
+        <img className='home-vector-image' src={homeVectorImage}></img>
       </div>
       <div className='forms-container-RIGHT'>
+        <img className='insta-logo' src={instaLogo}></img>
         {showSignUp ?
         <div className='signup-form-container'>
           <SignUpForm />
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -42,34 +42,30 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
+    <form className='login-form' onSubmit={onLogin}>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
         <input
+          className='login-input'
           name='email'
           type='text'
           placeholder='Email'
           value={email}
           onChange={updateEmail}
         />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
         <input
+          className='login-input'
           name='password'
           type='password'
           placeholder='Password'
           value={password}
           onChange={updatePassword}
         />
-        <button type='submit'>Login</button>
-        <button type='button' onClick={handleDemo}>Demo</button>
-      </div>
+      <button type='submit'>Login</button>
+      <button type='button' onClick={handleDemo}>Demo</button>
     </form>
   );
 };
