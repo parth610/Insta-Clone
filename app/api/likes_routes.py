585207e2f225532e59e6d5503409fceb8a754835
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-03 17:28:27 -0500

    postLikes post route

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import PostLike, User
+from app.models import PostLike, User, db
 
 likes_routes = Blueprint('post_likes', __name__)
 
@@ -10,5 +10,13 @@ def get_likes(id):
     return jsonify([like.to_dict() for like in likes])
 
 @likes_routes.route('/posts/<int:id>', methods=['POST'])
-def create_likes():
-    
+def create_like(id):
+    user_id = current_user.id
+    post_id = id
+    like = PostLike(
+        user_id,
+        post_id
+    )
+    db.session.add(like)
+    db.session.commit()
+    return like.to_dict()
