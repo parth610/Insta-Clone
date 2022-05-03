2e891cdf6bd2d770b753fee5fa70a12e353493ea
masontaylor7 <masonevantaylor@gmail.com>
2022-05-03 13:48:19 -0700

    suggested followers and post blocks fixed

--- react-app/src/components/LoadPosts/LoadPosts.js
+++ react-app/src/components/LoadPosts/LoadPosts.js
@@ -8,11 +8,14 @@ const LoadPosts = ({ posts }) => {
             {posts.map(post => (
                 <div key={post.id} className="loadPost__postCard">
                     <div className="loadPost__imageContainer">
-                        <img className='loadPost__image' src={post.image_url} alt={post.caption} />
-                        <div className="loadPost__contents">
+                        <div className='loadPost_opaque_container'>
+                            <img className='loadPost__image' src={post.image_url} alt={post.caption} />
                             <div className="loadPost__captionBlock">
-                                <p>{post.caption}</p>
+
+                                <div className='caption_text'>{post.caption}</div>
                             </div>
+                        </div>
+                        <div className="loadPost__contents">
                             <div className="loadPost__lowerLikes">
                                 <i className="fa-solid fa-heart fa-lg" ></i>
 
