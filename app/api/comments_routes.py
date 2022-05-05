a7987c278b77604730533b98d7b2efc9e670fb97
Gabriel Sitorus <sitorusgg@gmail.com>
2022-05-06 03:48:44 +0700

    comment routes

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -5,4 +5,11 @@ from app.forms import CommentForm
 
 comments_routes = Blueprint('comments', __name__)
 
+@comments_routes.route('/posts/<int:id>')
+def get_comment_by_post_id(id):
+    comments = Comment.query.filter(Comment.post_id == id)
+    return jsonify([comment.to_dict() for comment in comments])
 
+# @comments_routes.route('/posts/<int:id>')
+# def post_comment(id):
+#     form=CommentForm()
