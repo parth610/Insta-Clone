12022ee35433d72bd3f5f280e617f999c8e49233
masontaylor7 <masonevantaylor@gmail.com>
2022-05-05 10:42:30 -0700

    css for single post progress

---
+++ react-app/src/components/SinglePageCommentSection/SinglePageCommentSection.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import SinglePageCommentForm from '../SinglePageComment/SinglePageCommentForm';
+import '../SinglePageView/SinglePageView.css'
+
+const SinglePageCommentSection = () => {
+    return (
+        <div>
+            <SinglePageCommentForm />
+        </div>
+    )
+}
+
+export default SinglePageCommentSection
--- react-app/src/components/SinglePageView/SinglePageView.js
+++ react-app/src/components/SinglePageView/SinglePageView.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import SinglePageComment from '../SinglePageComment/SinglePageComment';
+import SinglePageCommentSection from '../SinglePageCommentSection/SinglePageCommentSection';
 import './SinglePageView.css'
 
-const SinglePageView = ({ select, handleClose }) => {
-    console.log(select);
+const SinglePageView = ({ select, setShowPage }) => {
+    const [deleteVerify, setDeleteVerify] = useState(false)
+
+    const handleClose = () => {
+        setShowPage(false)
+    }
+
     return (
         <div className="singlePageViewContainer" >
             <div className='singlePageView'>
-                <div className="singlePageView__image">
-                    <img className='singlePageView__pic' src={select.image_url} alt='' />
+                <div className='singlePageContents'>
+                    <div className="singlePageView__image" >
+                        <div className=''>
+                            <img className='singlePageView__pic' src={select.image_url} alt='' />
+
+                            <button type='button' className='close-post-view-button' onClick={handleClose}>
+                                {/* <i className="fa-solid fa-xmark fa-lg"></i> */}
+                                Close
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="singlePageView__commentSection">
+                        <SinglePageCommentSection />
+                    </div>
                 </div>
-            </div>
-            <div className="singlePageView__commentSection">
-                <SinglePageComment />
             </div>
         </div>
     )
